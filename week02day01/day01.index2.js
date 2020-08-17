59a202794654ce766390b54968ea3160f3f4379f
naresh_barfa <nareshbarfa007@gmail.com>
2020-08-17 16:33:25 +0530

    humming dis

---
+++ week02day01/day01.index2.js
@@ -0,0 +1,11 @@
+var arr = ["D", "B", "C", "E", "D"];
+
+    var counts = [];
+
+    for (var i = 0; i <= arr.length; i++) {
+        if (counts[arr[i]] === undefined) {
+            console.log(arr[1])
+        } 
+    }
+    
+
